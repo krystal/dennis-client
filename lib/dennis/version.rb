8d3925882414d41a8f7487bc5c359c5f95506a1d
Georgi Ganchev <googs.bs@gmail.com>
2025-04-01 09:55:04 +0100

    fix(ci): fix version string for releases

--- lib/dennis/version.rb
+++ lib/dennis/version.rb
@@ -2,11 +2,6 @@
 
 module Dennis
 
-  VERSION_FILE_ROOT = File.expand_path('../../VERSION', __dir__)
-  if File.file?(VERSION_FILE_ROOT)
-    VERSION = File.read(VERSION_FILE_ROOT).strip.sub(/\Av/, '')
-  else
-    VERSION = '1.2.3'
-  end
+  VERSION = '1.2.3'
 
 end
